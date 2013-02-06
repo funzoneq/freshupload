06e56e110130b5b3175f374e4fa30b48c0c1ae42
funzoneq <arnoud@tumblr.com>
2013-02-06 16:21:44 +0100

    Simple command line util

---
+++ client/upload.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import requests, sys, os
+from pprint import pprint
+
+def copy_to_clipboard (str):
+	cmd = 'echo %s | tr -d "\n" | pbcopy' % str
+	os.system(cmd)
+
+if len(sys.argv) < 2:
+    sys.stderr.write('Usage: sys.argv[0] image.jpg')
+    sys.exit(1)
+
+url = 'http://ops-arnoud-5f501c08.ewr01.tumblr.net/v1/api/upload'
+fn 	= sys.argv[1]
+
+r = requests.post(url, files={'photo': open(fn, 'rb')})
+result = r.json()
+if result['directurl']:
+	copy_to_clipboard(result['directurl'])
+	print result['directurl']
